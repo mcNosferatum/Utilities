3ede5c873fbf2a2a09a80f216f15cbefb48040a5
Evgentus <2557921@mail.ru>
2017-02-02 16:45:36 +0300

    Чек бокс для сохранения в файл

--- CreateWinUserPassword/CreateWinUserPassword/Form1.Designer.cs
+++ CreateWinUserPassword/CreateWinUserPassword/Form1.Designer.cs
@@ -31,13 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.tbUsers = new System.Windows.Forms.TextBox();
             this.cbTest = new System.Windows.Forms.CheckBox();
+            this.cbSaveToFile = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 23);
+            this.button1.Size = new System.Drawing.Size(120, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Change Pass";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,30 +46,42 @@
             // 
             // tbUsers
             // 
-            this.tbUsers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            this.tbUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.tbUsers.Location = new System.Drawing.Point(12, 44);
             this.tbUsers.Multiline = true;
             this.tbUsers.Name = "tbUsers";
             this.tbUsers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbUsers.Size = new System.Drawing.Size(258, 199);
+            this.tbUsers.Size = new System.Drawing.Size(258, 228);
             this.tbUsers.TabIndex = 1;
             // 
             // cbTest
             // 
             this.cbTest.AutoSize = true;
-            this.cbTest.Location = new System.Drawing.Point(182, 14);
+            this.cbTest.Location = new System.Drawing.Point(134, 13);
             this.cbTest.Name = "cbTest";
             this.cbTest.Size = new System.Drawing.Size(58, 21);
             this.cbTest.TabIndex = 2;
             this.cbTest.Text = "Test";
             this.cbTest.UseVisualStyleBackColor = true;
             // 
+            // cbSaveToFile
+            // 
+            this.cbSaveToFile.AutoSize = true;
+            this.cbSaveToFile.Location = new System.Drawing.Point(187, 13);
+            this.cbSaveToFile.Name = "cbSaveToFile";
+            this.cbSaveToFile.Size = new System.Drawing.Size(101, 21);
+            this.cbSaveToFile.TabIndex = 3;
+            this.cbSaveToFile.Text = "SaveToFile";
+            this.cbSaveToFile.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(282, 284);
+            this.Controls.Add(this.cbSaveToFile);
             this.Controls.Add(this.cbTest);
             this.Controls.Add(this.tbUsers);
             this.Controls.Add(this.button1);
@@ -84,6 +97,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox tbUsers;
         private System.Windows.Forms.CheckBox cbTest;
+        private System.Windows.Forms.CheckBox cbSaveToFile;
     }
 }
 
